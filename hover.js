7e2999a0ff0733b68549bfbfc33996aff2d351ec
Araya Wongwan <hi.arayaa@gmail.com>
2019-05-23 15:41:16 +0700

    hovering

---
+++ hover.js
@@ -0,0 +1,22 @@
+const link = document.querySelector(".signup h5")
+const circle = document.querySelector(".signup")
+
+circle.addEventListener("mouseover", function(){
+	link.style.color = "#ffffff"
+})
+
+circle.addEventListener("mouseout", function(){
+	link.style.color = "#100af2"
+})
+
+
+ const link2 = document.querySelector("a.signup.two h5")
+ const circle2 = document.querySelector(".signup.two")
+
+circle.addEventListener("mouseover", function(){
+	link2.style.color = "#ffffff"
+})
+
+circle.addEventListener("mouseout", function(){
+	link2.style.color = "#100af2"
+})
