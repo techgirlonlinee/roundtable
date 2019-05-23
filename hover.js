81452b9e8c68ef094835bb810b812f92f9d3e689
Araya Wongwan <hi.arayaa@gmail.com>
2019-05-23 21:07:49 +0700

    changes

--- hover.js
+++ hover.js
@@ -1,22 +1,10 @@
-const link = document.querySelector(".signup h5")
-const circle = document.querySelector(".signup")
+const link = document.querySelectorAll(".signup h5")
+const circle = document.querySelectorAll(".signup")
 
-circle.addEventListener("mouseover", function(){
+circles.forEach.addEventListener("mouseover", function(){
 	link.style.color = "#ffffff"
 })
 
-circle.addEventListener("mouseout", function(){
+circles.forEach.addEventListener("mouseout", function(){
 	link.style.color = "#100af2"
-})
-
-
- const link2 = document.querySelector("a.signup.two h5")
- const circle2 = document.querySelector(".signup.two")
-
-circle.addEventListener("mouseover", function(){
-	link2.style.color = "#ffffff"
-})
-
-circle.addEventListener("mouseout", function(){
-	link2.style.color = "#100af2"
 })
