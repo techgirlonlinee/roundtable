8a4a69f1502d5df7df060f8869265ff9ac168a0b
Araya Wongwan <hi.arayaa@gmail.com>
2019-06-08 17:33:43 +0700

    mobile version for three pages

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,5 @@
 const button = document.querySelector(".menu-toggle")
+const buttonMobile = document.querySelector(".menu-toggle-mobile")
 const menuBar = document.querySelector("div.menu")
 const button2 = document.querySelector(".menu-slider")
 
@@ -6,7 +7,12 @@ button.addEventListener("click", function(){
 	menuBar.classList.add("open")
 })
 
+buttonMobile.addEventListener("click", function(){
+	menuBar.classList.add("open")
+})
 
 button2.addEventListener("click", function(){
 	menuBar.classList.remove("open")
 })
+
+
